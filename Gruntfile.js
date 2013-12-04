a7a82dd110e66b7efa1dfdc0d889fe10dfa2446d
Lucas <lucas@agtlucas.com>
2013-12-04 21:43:48 -0200

    Learning Compass first

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,36 +1,56 @@
-// 'use strict';
+'use strict';
 
-// module.exports = function(grunt) {
+module.exports = function(grunt) {
 
-//   // Project configuration
-//   grunt.initConfig({
+  // Project configuration
+  grunt.initConfig({
 
-//     // Read package.json file
-//     pkg: grunt.file.readJSON('package.json'),
+    // Read package.json file
+    pkg: grunt.file.readJSON('package.json'),
 
-//     // Define paths
-//     meta: {
-//       srcPathSass: 'assets/sass/',
-//       srcPathJS: 'assets/js/',
-//       buildPathCSS: 'build/css/',
-//       buildPathJS: 'build/js/'
-//     },
+    // Define paths
+    meta: {
+      srcPathSass: 'src/sass/',
+      srcPathJS: 'src/js/',
+      buildPathCSS: 'build/css/',
+      buildPathJS: 'build/js/'
+    },
 
-//     // Sass
-//     sass: {
-//       dist: {
-//         // Grab the main.scss and make the main.css
-//         files: {
-//           '<%= meta.buildPathCSS %>main.css' : '<%= meta.srcPathSass %>main.scss'
-//         },
-//         // Minify Sass
-//         options: {
-//           style: 'compressed'
-//         }
-//       }
-//     },
+    // // Sass
+    // sass: {
+    //   dist: {
+    //     // Grab the main.scss and make the main.css
+    //     files: {
+    //       '<%= meta.buildPathCSS %>main.css' : '<%= meta.srcPathSass %>main.scss'
+    //     },
+    //     // Minify Sass
+    //     options: {
+    //       style: 'compressed'
+    //     }
+    //   }
+    // },
 
+    compass: {
+      dist: {
+        options: {
+          require: 'susy',
+          sassDir: '<% meta.srcPathSass %>',
+          cssDir: '<% meta.buildPathSass %>',
+          environment: 'production'
+        }
+      },
+      dev: {
+        options: {
+          require: 'susy',
+          sassDir: '<% meta.srcPathSass %>',
+          cssDir: '<% meta.buildPathSass %>'
+        }
+      }
+    },
 
+  });
 
-//   });
-// }
+  grunt.loadNpmTasks('grunt-contrib-compass');
+
+  grunt.registerTask('default', ['compass']);
+}
