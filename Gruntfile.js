c1b864263ec516bbf3d66066dfb108fc74b1aca5
Lucas <lucas@agtlucas.com>
2013-12-05 22:59:51 -0200

    Update Grunt and package

--- Gruntfile.js
+++ Gruntfile.js
@@ -74,6 +74,10 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
 
-  grunt.registerTask('default', ['compass']);
+  grunt.registerTask('default', ['connect', 'watch']);
 }
